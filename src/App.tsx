3fd918998eb736ad6ea3cccb2d4633af8cda37ed
wilgnerl <wilgnerl@al.insper.edu.br>
2022-12-28 00:29:21 -0300

    feat: Add components in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-
+import { AddNewTask } from "./components/AddNewTask"
+import { Header } from "./components/Header"
 import "./global.css"
 
 export function App() {
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <AddNewTask />
     </div>
   )
 }
