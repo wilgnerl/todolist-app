7a490e7aac18515c732ae832a41756c8da251292
wilgnerl <wilgnerl@al.insper.edu.br>
2022-12-29 21:43:52 -0300

    feat: response app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header"
 import { TasksPanel } from "./components/TasksPanel"
+import styles from "./App.module.css"
+
 import "./global.css"
 
 export function App() {
 
   return (
-    <div className="App">
+    <div className={styles.app}>
       <Header />
       <TasksPanel />
     </div>
